4e822edf0eaf61068a9e8960ec76f49368cf5a66
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-16 23:18:16 +0000

    code starts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       <h1>hi yt</h1>
+    <div className="bg-green-900">
+     HIII
     </div>
   );
 }
