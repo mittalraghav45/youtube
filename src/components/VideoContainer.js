4865f276d60dbabd81c559d356c98a36581e8b4e
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-17 15:19:21 +0000

    api issue resolved

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 import VideoCards from "./VideoCards";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
@@ -10,17 +11,19 @@ const VideoContainer = () => {
 
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
-    const json = await data.json(); 
+    const json = await data.json();
     setVideos(json.items);
   };
+
   return (
-    <div className='flex flex-wrap'>
-      {videos.map((video)=>
-      <VideoCards
-      key={video.id} info={video}/>)}
-      
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <Link to={'/watch?v='+video.id}  >
+          <VideoCards  key={video.id} 
+          info={video} />
+        </Link>
+      ))}
     </div>
   );
 };
-
 export default VideoContainer;
