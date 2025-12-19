2a630e6504450e321913997a72fc65b1cee97163
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-19 22:27:17 +0000

    adding searchSLice

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,6 @@
 import{configureStore} from '@reduxjs/toolkit'
 import appSlice from './appSlice';
-import searchSlice from './appSlice';
+import searchSlice from './searchSlice';
 const store=configureStore({
 reducer:{
     app:appSlice,
