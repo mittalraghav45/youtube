0934c757206a67a5bcd90c36d95f2a883fb5e15f
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-19 23:11:33 +0000

    working on Comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -12,6 +13,7 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
+    <> 
     <div className="px-5"> 
     <iframe
       width="1000"
@@ -22,6 +24,8 @@ const WatchPage = () => {
       allowFullScreen
     ></iframe>
     </div>
+    <CommentsContainer/>
+    </>
   );
 };
 
