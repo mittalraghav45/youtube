c07ed490cc54665440e392798597d32713f752f7
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-20 22:03:47 +0000

    livechat feature stRTS

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,28 +3,34 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"));
+  // console.log(searchParams.get("v"));
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="flex flex-col"> 
-    <div className="px-5"> 
-    <iframe
-      width="1000"
-      height="500"
-      src={"https://www.youtube.com/embed/"+searchParams.get("v")}
-      title="YouTube video player"    
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen
-    ></iframe>
-    </div>
-    <CommentsContainer/>
+    <div className="flex flex-col">
+      <div className="px-5 flex">
+        <div className="">
+          <iframe
+            width="1000"
+            height="500"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div>
+          <LiveChat />
+        </div>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
