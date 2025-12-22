396408d22fa2b416e7726439dd959b7e846fea36
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-22 11:44:28 +0000

    using GB for api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const OFFSET=10;
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+GOOGLE_API_KEY;
+  "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=GB&maxResults=50&key="+GOOGLE_API_KEY;
  
 export const YOUTUBE_SEARCH_API='https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 
